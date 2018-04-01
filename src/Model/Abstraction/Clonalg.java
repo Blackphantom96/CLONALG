b6694c4b7760d7cfa37dcd778fac3996218519c9
Alejandro Anzola <anzola95avila@gmail.com>
2018-04-01 16:40:37 -0500

    implementa afinidad "hammingComplement"

--- src/Model/Abstraction/Clonalg.java
+++ src/Model/Abstraction/Clonalg.java
@@ -1,41 +1,52 @@
 package Model.Abstraction;
 
 public abstract class Clonalg<E> {
-	
+
 	public void main(AntiBody<E>[] ab, Antigen<E>[] ag, int Ngen, int n, int d, int L, double beta) {
-		AntiBody<E> [] abm = fillRandomMemory(ab, ag.length);
-		AntiBody<E> [] abr = null ; //FIXME ver que se hace
-		for(int i = 0; i<Ngen;i++) {
+		AntiBody<E>[] abm = fillRandomMemory(ab, ag.length);
+		AntiBody<E>[] abr = null; // FIXME ver que se hace
+		for (int i = 0; i < Ngen; i++) {
 			double[][] affin = new double[ag.length][ab.length];
-			AntiBody<E> [][] abn = null ;
-			for(int j =0; j <ag.length; j++) { //TODO Poner al antigeno y al anticuerpo .size()
+			AntiBody<E>[][] abn = null;
+			for (int j = 0; j < ag.length; j++) { // TODO Poner al antigeno y al anticuerpo .size()
 				affin[j] = affinity(ab, ag[j]);
-				abn[j] = select(ab, affin[j], n);//TODO posiblemente sobra 
-				AntiBody<E>[] C = clone(ab,beta, affin[j]);
-				mutate(C,affin[j]);
-				double[] affinP = affinity(C	, ag[j]);
+				abn[j] = select(ab, affin[j], n);// TODO posiblemente sobra
+				AntiBody<E>[] C = clone(ab, beta, affin[j]);
+				mutate(C, affin[j]);
+				double[] affinP = affinity(C, ag[j]);
 				AntiBody<E> selected = select(C, affinP, 1)[0];
-				if( affinity(selected,ag[j]) > affinity(abm[j],ag[j]) ) //TODO ver si m es el tamaño de la memoria o es M
+				if (calculateAffinity(selected, ag[j]) > calculateAffinity(abm[j], ag[j])) // TODO ver si m es el tamaño
+																							// de la memoria o
+					// es M
 					abm[j] = selected;
 				AntiBody<E>[] abd = generate(d, L);
-				replace(abr,abd,affin);
+				replace(abr, abd, affin);
 			}
-		}		
+		}
 	}
-	
+
 	public abstract double[] affinity(AntiBody<E>[] ab, Antigen<E> sag);
-	public abstract AntiBody<E> []  fillRandomMemory(AntiBody<E>[] ab, int x);
+
+	public abstract AntiBody<E>[] fillRandomMemory(AntiBody<E>[] ab, int x);
+
 	/**
-	 * @param ab un arreglo de anticuerpos ORDENADO por su afinidad con el antigeno en revision
-	 * @param aff 
+	 * @param ab
+	 *            un arreglo de anticuerpos ORDENADO por su afinidad con el antigeno
+	 *            en revision
+	 * @param aff
 	 * @param n
-	 * @return 
+	 * @return
 	 */
-	public abstract AntiBody<E>[] select(AntiBody<E>[] ab, double[] aff, int n );
+	public abstract AntiBody<E>[] select(AntiBody<E>[] ab, double[] aff, int n);
+
 	public abstract AntiBody<E>[] clone(AntiBody<E>[] ab, double beta, double[] aff);
+
 	public abstract void mutate(AntiBody<E>[] ab, double[] aff);
-	public abstract AntiBody<E>[] generate(int d, int L); 
-	public abstract void replace (AntiBody<E>[] ab, AntiBody<E>[] ab1, double[][] affin);
-	public abstract double affinity(AntiBody<E> ab, Antigen<E> sag);
-	
+
+	public abstract AntiBody<E>[] generate(int d, int L);
+
+	public abstract void replace(AntiBody<E>[] ab, AntiBody<E>[] ab1, double[][] affin);
+
+	public abstract double calculateAffinity(AntiBody<E> ab, Antigen<E> sag);
+
 }
--- src/Model/impl/ClonalgImpl.java
+++ src/Model/impl/ClonalgImpl.java
@@ -1,46 +1,86 @@
 package Model.impl;
 
+import java.util.Map;
+
 import Model.Abstraction.*;
 
-public class ClonalgImpl extends Clonalg<boolean[]>{
+public class ClonalgImpl extends Clonalg<boolean[]> {
+
+	public static final String DEFAULT_AFFINITY_FUNCTION = "hammingComplement";
 	
-	@Override
-	public double[] affinity(AntiBody<boolean[]>[] ab, Antigen<boolean[]> sag){
-		return null;
+	private interface AffinityFunction {
+		public double calculate(AntiBody<boolean[]> ab, Antigen<boolean[]> ag);
 	}
+
+	static Map<String, AffinityFunction> functions;
+
+	public ClonalgImpl() {
+		generateAffinityFunctions();
+	}
+	
+	public void generateAffinityFunctions() {
+		functions.put("hammingComplement", (ab, ag) -> {
+			int r = 0;
+			boolean abData[] = ab.getData();
+			boolean agData[] = ag.getData();
+			assert abData.length == agData.length;
+
+			for (int i = 0; i < agData.length; i++) {
+				r += (abData[i] == agData[i] ? 0 : 1);
+			}
+			return (double) r;
+		});
+	}
+
 	@Override
-	public AntiBody<boolean[]> []  fillRandomMemory(AntiBody<boolean[]>[] ab, int x){
+	public double[] affinity(AntiBody<boolean[]>[] ab, Antigen<boolean[]> sag) {
+		int size = ab.length;
+		double[] r = new double[size];
+		for (int i = 0; i < size; i++) {
+			r[i] = calculateAffinity(ab[i], sag);
+		}
+
+		return r;
+	}
+
+	@Override
+	public AntiBody<boolean[]>[] fillRandomMemory(AntiBody<boolean[]>[] ab, int x) {
 		return null;
 	}
 
 	@Override
-	public AntiBody<boolean[]>[] select(AntiBody<boolean[]>[] ab, double[] aff, int n ){
+	public AntiBody<boolean[]>[] select(AntiBody<boolean[]>[] ab, double[] aff, int n) {
 		AntiBody<boolean[]>[] res = new AntiBody[n];
-		for (int i=0; i<n ;i++) 
+		for (int i = 0; i < n; i++)
 			res[i] = ab[i];
 		return res;
 	}
+
 	@Override
-	public AntiBody<boolean[]>[] clone(AntiBody<boolean[]>[] ab, double beta, double[] aff){
+	public AntiBody<boolean[]>[] clone(AntiBody<boolean[]>[] ab, double beta, double[] aff) {
 		AntiBody<boolean[]>[] res = new AntiBody[ab.length];
-		for (int i=0; i<ab.length ;i++) 
+		for (int i = 0; i < ab.length; i++)
 			res[i] = ab[i].clone();
 		return null;
 	}
+
 	@Override
-	public void mutate(AntiBody<boolean[]>[] ab, double[] aff){
-		
+	public void mutate(AntiBody<boolean[]>[] ab, double[] aff) {
+
 	}
+
 	@Override
-	public AntiBody<boolean[]>[] generate(int d, int L){
+	public AntiBody<boolean[]>[] generate(int d, int L) {
 		return null;
-	} 
-	@Override
-	public void replace (AntiBody<boolean[]>[] ab, AntiBody<boolean[]>[] ab1, double[][] affin){
-		
 	}
+
 	@Override
-	public double affinity(AntiBody<boolean[]> ab, Antigen<boolean[]> sag){
-		return (Double) null;
+	public void replace(AntiBody<boolean[]>[] ab, AntiBody<boolean[]>[] ab1, double[][] affin) {
+
+	}
+
+	@Override
+	public double calculateAffinity(AntiBody<boolean[]> ab, Antigen<boolean[]> sag) {
+		return functions.get(DEFAULT_AFFINITY_FUNCTION).calculate(ab, sag);
 	}
 }
